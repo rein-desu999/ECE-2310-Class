3e83ce73124c97a7825bd1cf11833233e6f8b61d
rein-desu999 <122199781+rein-desu999@users.noreply.github.com>
2025-02-25 14:37:20 -0800

    commit

--- Comprehence2/main.cpp
+++ Comprehence2/main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 int main(){
     Pet overloaded = Pet("Dog", 5, "John", true);
+    Pet unknown = Pet();
+    printf("Name: %s \tAge: %i \tOwner: %s \tHouse Trained: %s\n", unknown.getname().c_str(), unknown.getage(), unknown.getowner().c_str(), unknown.getisHouseTrained()? "Yes":"No");
     printf("Name: %s \tAge: %i \tOwner: %s \tHouse Trained: %s\n", overloaded.getname().c_str(), overloaded.getage(), overloaded.getowner().c_str(), overloaded.getisHouseTrained()? "Yes":"No");
     overloaded.updateName("Cat");
     overloaded.updateAge();
     overloaded.updateOwner("Jane");
     overloaded.updateHouseTrained();
     printf("Name: %s \tAge: %i \tOwner: %s \tHouse Trained: %s\n", overloaded.getname().c_str(), overloaded.getage(), overloaded.getowner().c_str(), overloaded.getisHouseTrained() ? "Yes" : "No");
-    return 0;
+    return 0;   
 }
